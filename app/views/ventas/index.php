693f8782f6b9bb00fe4f5832aadb35b56dfa7895
luis D' Lima <delimce@gmail.com>
2013-04-01 22:58:50 -0430

    eliminando carpetas innecesarias

--- app/views/ventas/index.php
+++
@@ -1,3 +0,0 @@
-<div style="alignment-adjust: middle">
-    
-</div>
--- web/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: luis
- * Date: 7/18/12
- * Time: 6:39 PM
- * To change this template use File | Settings | File Templates.
- */
